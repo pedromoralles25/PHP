c3e4ef46951e13883fc9eb15313ee4f9046d317e
Larissa Ferreira <larissaferreirabarbosa31@gmail.com>
2022-09-23 22:11:49 -0300

    Update create.sql

--- Layout/create.sql
+++ Layout/create.sql
@@ -1,5 +1,5 @@
 CREATE USER 'admin'@'localhost' IDENTIFIED BY '123'; 
-GRANT ALL PRIVILEGES ON ds302.* TO 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON bdsql.* TO 'admin'@'localhost';
 
 CREATE TABLE paciente ( 
     cod_usuario int NOT NULL AUTO_INCREMENT, 
@@ -9,5 +9,17 @@ CREATE TABLE paciente (
     telefone varchar(15) NOT NULL,
     email varchar(30) NOT NULL, 
     senha varchar(30) NOT NULL, 
-    primary key(cod_usuario) 
+    primary key(cod_paciente) 
+);
+
+CREATE TABLE funcionario ( 
+    cod_usuario int NOT NULL AUTO_INCREMENT, 
+    nome varchar(30) NOT NULL, 
+    crm varchar(20) NOT NULL, 
+    especialidade varchar(20) NOT NULL, 
+    data_nasc varchar(10) NOT NULL, 
+    telefone varchar(15) NOT NULL,
+    email varchar(30) NOT NULL, 
+    senha varchar(30) NOT NULL, 
+    primary key(cod_funcionario) 
 );
