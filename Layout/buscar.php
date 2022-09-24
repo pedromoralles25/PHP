a6f7603a1fdc20988cd9cf0d993721b5fc9e4ce8
Larissa Ferreira <larissaferreirabarbosa31@gmail.com>
2022-09-24 15:45:54 -0300

    Update buscar.php

--- Layout/buscar.php
+++ Layout/buscar.php
@@ -1,50 +1,54 @@
- <?php
-            
-            $operacao = $_REQUEST ["operacao"]
+<html>
+            <body>
+                <head>
+                    <title>Buscar</title>
+                </head>
+                <div class="block">
+                    <div class='container'>
+                        <h1>Tabela Paciente</h1>
+                        <table class='table'>
+                            <thead>
+                                <tr>
+                                    <th>Nome</th>
+                                    <th>CPF
+                                    <th>Telefone</th>
+                                    <th>Data de Nascimento</th>
+                                    <th>Email</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                
+                                <?php
+                
+                                $operacao = $_REQUEST ["operacao"]
+                                
+                                if($operacao== "buscar"){
+                                    $nome = $_POST["nome"];
+                                    $sql= "SELECT * FROM cliente WHERE nome like '%$nome%';";
+                                    $res= mysqli_query($mysqli,$sql);
+                                    $linhas= mysqli_num_rows($res);
 
-            if($operacao== "buscar"){
-            $nome = $_POST["nome"];
-            $sql= "SELECT * FROM cliente WHERE nome like '%$nome%';";
-            $res= mysqli_query($mysqli,$sql);
-            $linhas= mysqli_num_rows($res);
-            ?>
-            
-            <div class="block">
-                <div class='container'>
-                    <h1>Tabela Paciente</h1>
-                    <table class='table'>
-                    <thead>
-                    <tr>
-                    <th></th>
-                    <th>Nome</th>
-                    <th>CPF
-                    <th>Telefone</th>
-                    <th>Data de Nascimento</th>
-                    <th>Email</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-
-              <?php
-
-                $sql= "SELECT * FROM paciente";
-                $res= mysqli_query($mysqli,$sql);
-                $linhas= mysqli_num_rows($res);
-
-                for ($i = 0; $i < $linhas; $i++){
-                    $paciente = mysqli_fetch_array ($res);
-                    
-                    echo"
-                        <tr>
-                        <td>".$paciente['nome']."</td>
-                        <td>".$paciente['cpf']."</td>
-                        <td>".$paciente['telefone']."</td>
-                        <td>".$paciente['data_nasc']."</td>
-                        <td>".$paciente['email']."</td>
-                        </tr>";
-                        }
-                        ?>
-                    </tbody>
-                    </table>
+                                    $sql= "SELECT * FROM paciente";
+                                    $res= mysqli_query($mysqli,$sql);
+                                    $linhas= mysqli_num_rows($res);
+                                    
+                                    for ($i = 0; $i < $linhas; $i++){
+                                        $paciente = mysqli_fetch_array ($res);
+                                        
+                                        echo"
+                                            <tr>
+                                            <td>".$paciente['nome']."</td>
+                                            <td>".$paciente['cpf']."</td>
+                                            <td>".$paciente['telefone']."</td>
+                                            <td>".$paciente['data_nasc']."</td>
+                                            <td>".$paciente['email']."</td>
+                                            </tr>";
+                                            }
+                                        }
+                                        ?>
+                            </tbody>
+                            </table>
                     </div>
-                    </div>
+                </div>
+            </body>
+            </html>
